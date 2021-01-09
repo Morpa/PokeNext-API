f2ff86c253822f776623581780dd710641448fd6
Andre Morpa <andremorpa@gmail.com>
2021-01-09 09:53:15 +0000

    Add Api types

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export { default as ApiResult } from './api';
+export { default as Pokemon } from './pokemon';
+export { default as Specie } from './specie';
+export { default as Evolution } from './evolution';
+export { default as Type } from './type';
